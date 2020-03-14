7bac818b4b24ed6f9193a7477754e155a2422d33
supriyaranjane <supriyaranjane6@gmail.com>
2020-03-14 11:21:18 +0530

    Addded:Finding the Day of Month in Between march 3 to March 6

---
+++ IfSelctionPractice/DayAndMonth.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -x 
+
+args=("$@")
+
+echo $@
+day=${args[0]}
+month=${args[1]}
+#day=${args[0]}
+
+if [ $month -eq 3 ];
+then
+		if [ $day -ge 20 ] || [ $day -le 31 ] 
+		then
+				echo true
+		else
+				echo false
+		fi
+fi
+if [ $month -eq 4 ];
+then
+      if [ $day -ge 1 ] || [ $day -le 30 ] 
+      then
+            echo true
+      else
+            echo false
+      fi
+
+fi
+if [ $month -eq 5 ];
+then 
+		if [ $day -ge 1 ] || [ $day -le 31 ] 
+      then
+            echo true
+      else
+            echo false
+      fi
+fi
+if [ $month -eq 6 ];
+then
+ 		if [ $day -ge 1 ] || [ $day -le 20 ] 
+      then
+            echo true
+      else
+            echo false
+      fi
+	
+fi
+
+
+
+
