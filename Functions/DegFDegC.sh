2566f1847b45bea50db0cc00826d9c1f0140ab0f
supriyaranjane <supriyaranjane6@gmail.com>
2020-03-16 19:18:13 +0530

    Added:Findin the MagicNumber

---
+++ Functions/DegFDegC.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+function  getDegF(){
+	read -p "enter the temperature in celcius: " degC
+	degF=$(( ($degC*9/5) + 32 ))
+}
+
+function getDegC(){
+	read -p "enter the temperature in farenheit: " degF
+	degC=$(( ($degF-32) * 5/9 ))
+}
+
+read -p "calculating the temperature in farenheit press '1' calculating the temperature in celcius press '2'" ch
+
+case $ch in
+	1)
+		$(getDegF)
+		;;
+	2)
+		$(getDegC)
+		;;
+esac
